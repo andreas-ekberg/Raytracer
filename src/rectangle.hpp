d70cba4f110685f1f977fd748476da60bbdb3e5a
Andreas Ekberg <112385508+andreas-ekberg@users.noreply.github.com>
2024-09-09 08:05:45 +0200

    Format

--- src/rectangle.hpp
+++ src/rectangle.hpp
@@ -3,39 +3,44 @@
 #include "glmlib/glm.hpp"
 #include "glmlib/gtx/string_cast.hpp"
 
-class Rectangle : public Polygon {
+class Rectangle : public Polygon
+{
 public:
     // Constructor
     Rectangle() {}
-    Rectangle(glm::vec3 topLeft, glm::vec3 topRight, glm::vec3 bottomLeft, glm::vec3 bottomRight, glm::vec3 _color) {
+    Rectangle(glm::vec3 topLeft, glm::vec3 topRight, glm::vec3 bottomLeft, glm::vec3 bottomRight, glm::vec3 _color)
+    {
         p[0] = topLeft;
         p[1] = topRight;
         p[2] = bottomLeft;
         p[3] = bottomRight;
         color = _color;
-        normal = glm::cross(p[1] - p[0], p[2] - p[0]);
+        normal = glm::cross(p[0] - p[2], p[3] - p[2]);
     }
-
 
     // Destructor
     ~Rectangle() {}
 
-    bool isHit(Ray &r, float &t) {
+    bool isHit(Ray &r, float &t)
+    {
         // First time t is null
-        if (glm::dot(r.direction(), normal) < 0){
+        if (glm::dot(r.direction(), normal) < 0)
+        {
             return false;
         };
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++)
+        {
             glm::vec3 pos = r.position((float)i);
-            if (pos.x >=p[0].x && pos.x <= p[1].x && pos.z <= p[0].z && pos.z >= p[2].z && pos.y - p[0].y < 1) {
+            if (pos.x >= p[0].x && pos.x <= p[1].x && pos.z <= p[0].z && pos.z >= p[2].z && pos.y - p[0].y < 1)
+            {
                 t = i;
                 return true;
             }
         }
-        return false; 
+        return false;
     }
-private:   
+
+private:
     glm::vec3 p[4];
     glm::vec3 color;
-
 };
