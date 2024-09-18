f5b031d411e16578238f96fa14a8d2fd7f5d3305
Gustav Johansson <xgujoh1999@gmail.com>
2024-09-18 11:24:52 +0200

    Create sphere.h

---
+++ src/headers/sphere.h
@@ -0,0 +1,23 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "../glmlib/glm.hpp"
+#include "../glmlib/gtx/string_cast.hpp"
+#include "../ray.hpp"
+
+class Sphere {
+public:
+    Sphere();
+    Sphere(const glm::dvec3& center, float radius);
+
+    bool hit(const Ray& r, float t_min, float t_max) const;
+
+    glm::dvec3 center() const;
+    float radius() const;
+
+private:
+    glm::dvec3 m_center;
+    float m_radius;
+};
+
+#endif // SPHERE_H
